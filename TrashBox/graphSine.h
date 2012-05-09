68049acf39ef5812ea6adaf65854f3d23a0ce390
Michael Siegel <pomplesiegel@gmail.com>
2012-05-09 01:14:59 -0400

    dan?

---
+++ TrashBox/graphSine.h
@@ -0,0 +1,13 @@
+//
+//  graphSine.h
+//  TrashBox
+//
+//  Created by Dan Raisbeck on 5/9/12.
+//  Copyright (c) 2012 Tufts University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface graphSine : UIView
+
+@end
